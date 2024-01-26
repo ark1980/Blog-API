042de8abb9c337980201bc994ec348b3d28f7dcf
Ali Keshanian <ar.k@outlook.com>
2024-01-26 09:28:37 -0800

    Add PATCH endpoint to update posts by ID

--- index.js
+++ index.js
@@ -66,8 +66,19 @@ app.post("/posts", (req, res) => {
   posts.push(newPost);
   res.status(201).json(newPost);
 });
-//CHALLENGE 4: PATCH a post when you just want to update one parameter
 
+//CHALLENGE 4: PATCH a post when you just want to update one parameter
+app.patch("/posts/:id", (req, res) => {
+  const postId = parseInt(req.params.id);
+  const post = posts.find((p) => p.id === postId);
+  if (!post) return res.status(404).json({ message: "Post not found" });
+
+  if (req.body.title) post.title = req.body.title;
+  if (req.body.content) post.content = req.body.content;
+  if (req.body.author) post.author = req.body.author;
+
+  res.json(post);
+});
 //CHALLENGE 5: DELETE a specific post by providing the post id.
 
 app.listen(port, () => {
